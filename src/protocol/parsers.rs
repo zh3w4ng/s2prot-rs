b31a0989ececd784c0afac182923c016ca54cc12
Zhe Wang <zh3w4ng@gmail.com>
2025-01-11 22:19:06 -0800

    add parse_imports and parse_blank_lines

--- src/protocol/parsers.rs
+++ src/protocol/parsers.rs
@@ -1,27 +1,35 @@
 extern crate nom;
 
 use nom::{
+    bytes::complete::{tag, take_till, take_until},
+    character::complete::{line_ending, newline},
+    multi::many0,
+    sequence::preceded,
     IResult,
-    bytes::complete::{
-        tag,
-        take_till
-    },
-    character::{
-        is_newline,
-        complete::line_ending,
-    },
-    multi::many0
 };
 
 pub fn parse_comments(input: &str) -> IResult<&str, &str> {
     let (input, _) = many0(line_ending)(input)?;
     let (input, _) = tag("#")(input)?;
     let (input, comment) = take_till(|c| c == '\n')(input)?;
-    //let (input, _) = newline(input)?;
+    let (input, _) = newline(input)?;
 
-    Ok((input, comment))
+    Ok((input, comment.trim_end()))
 }
 
+pub fn parse_imports(input: &str) -> IResult<&str, (&str, &str)> {
+    let (input, import_from) = preceded(tag("from "), take_until(" import "))(input)?;
+    // let (input, _) = tag(" import ")(input)?;
+    let (input, imported) = preceded(tag(" import "), take_until("\n"))(input)?;
+
+    Ok((input, (import_from, imported.trim_end())))
+}
+
+pub fn parse_blank_lines(input: &str) -> IResult<&str, &str> {
+    let (input, _) = many0(line_ending)(input)?;
+
+    Ok((input, ""))
+}
 
 #[cfg(test)]
 mod tests {
@@ -31,27 +39,43 @@ mod tests {
     fn it_parses_comments_with_no_error() {
         let input = r#"
 # Copyright (c) 2015-2017 Blizzard Entertainment
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+
+
+
+# Decoding instructions for each protocol type.
+typeinfos = [
+    ('_int',[(0,7)]),  #0
+    ('_int',[(0,4)]),  #1
+    ('_int',[(0,5)]),  #2
+    ('_int',[(0,6)]),  #3
+]
         "#;
-        let Ok((_, comment)) = parse_comments(input) else { panic!("parse_comments failed.")};
-        assert_eq!(comment.trim_end(), " Copyright (c) 2015-2017 Blizzard Entertainment");
-        //assert_eq!(rest.len(), 100);
+        let Ok((_, comment)) = parse_comments(input) else {
+            panic!("parse_comments failed.")
+        };
+        assert_eq!(comment, " Copyright (c) 2015-2017 Blizzard Entertainment");
+    }
+
+    #[test]
+    fn it_parse_imports_with_no_error() {
+        let input = r#"from s2protocol.decoders import *
+        "#;
+        let Ok((_, (import_from, imported))) = parse_imports(input) else {
+            panic!("parse_imports failed.")
+        };
+        assert_eq!(import_from, "s2protocol.decoders");
+        assert_eq!(imported, "*");
+    }
+
+    #[test]
+    fn it_parse_blank_lines_with_no_error() {
+        let input = r#"
+
+abc"#;
+        let Ok((input, res)) = parse_blank_lines(input) else {
+            panic!("parse_blank_lines failed.")
+        };
+        assert_eq!(input, "abc");
+        assert_eq!(res, "");
     }
 }
