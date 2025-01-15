ebe6bea9e34ea44405156c3c639f8efb8e840012
Zhe Wang <zh3w4ng@gmail.com>
2025-01-14 20:56:07 -0800

    add parser for EventType and create Protocol struct as the container

--- src/protocol/types.rs
+++ src/protocol/types.rs
@@ -5,6 +5,13 @@ pub struct Field {
 }
 
 #[derive(Debug, PartialEq)]
+pub struct EventType {
+    pub event_name: String,
+    pub event_id: u16,
+    pub type_index: u16,
+}
+
+#[derive(Debug, PartialEq)]
 pub enum TypeInfo {
     Int {
         offset: usize,
@@ -31,4 +38,23 @@ pub enum TypeInfo {
     Struct {
         fields: Vec<Field>,
     },
+    FourCC,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Protocol {
+    pub build_version: u16,
+    pub typo_infos: Vec<TypeInfo>,
+    pub has_tracker_events: bool,
+    pub game_events_types: Vec<EventType>,
+    pub game_eventid_type_index: u16,
+    pub message_events_types: Vec<EventType>,
+    pub message_eventid_type_index: u16,
+    pub tracker_events_types: Vec<EventType>,
+    pub tracker_eventid_type_index: u16,
+    pub game_loop_type_index: usize,
+    pub replay_userid_type_index: u16,
+    pub replay_header_type_index: u16,
+    pub game_details_type_index: u16,
+    pub replay_initdata_type_index: u16,
 }
