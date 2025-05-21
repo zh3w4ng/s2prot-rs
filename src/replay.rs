2ffe5192d2bef12794d52039e285a49e057ecf9e
Zhe Wang <zh3w4ng@gmail.com>
2025-05-20 19:32:27 -0700

    extract fn decode_user_data

--- src/replay.rs
+++ src/replay.rs
@@ -8,20 +8,27 @@ use decoder::Decoder;
 use mpq::Archive;
 use types::*;
 
-pub fn build_replay(file_name: &str, protocol: &Protocol) -> ParsedField {
+pub fn build_replay(file_name: &str, protocol: &Protocol) -> Vec<ParsedField> {
     let mut archive = load_mpq_archive(file_name);
     let user_data = archive
         .read_user_data()
         .unwrap()
         .expect("Failed to retrieve User Data");
+
+    let parsed_user_data = decode_user_data(&user_data, protocol);
+    vec![parsed_user_data]
+}
+
+fn load_mpq_archive(file_name: &str) -> Archive {
+    Archive::open(file_name).expect("Failed to open MPQ archive")
+}
+
+fn decode_user_data(user_data: &[u8], protocol: &Protocol) -> ParsedField {
     let index: usize = protocol.replay_header_type_index.unwrap();
-    let bit_packed_buffer = BitPackedBuff::new_big_endian(&user_data);
+    let bit_packed_buffer = BitPackedBuff::new_big_endian(user_data);
+
     match Decoder::new(bit_packed_buffer).decode("UserData", index, protocol) {
         Ok((_, parsed_field)) => parsed_field,
         _ => panic!("Failed to parse user data"),
     }
-}
-
-pub fn load_mpq_archive(file_name: &str) -> Archive {
-    Archive::open(file_name).expect("Failed to open MPQ archive")
 }
